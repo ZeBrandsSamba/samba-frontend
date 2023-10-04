1599345bb2821b3251878ae78a50d361643ef982
Oscar Meza Aldama <oscar.meza@zeb.mx>
2023-10-04 09:10:45 -0600

    Use web history mode for vue router

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
+import { createRouter, createWebHistory } from 'vue-router';
 
 import Home from '../views/Home.vue';
 import Survey from '../views/Survey.vue';
@@ -15,6 +15,6 @@ const routes = [
 ];
 
 export const router = createRouter({
-    history: createWebHashHistory(),
+    history: createWebHistory(),
     routes,
 });
